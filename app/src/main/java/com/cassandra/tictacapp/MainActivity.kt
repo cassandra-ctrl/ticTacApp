3a7fc92349c742312ca028c8f86419df179b9fc1
cass <d080803lopez@gmail.com>
2024-12-26 22:58:24 -0600

    Se hicieron cambios en la funcion nextScreen(), ya que no nos permitía cambiar de una actividad a otra

--- app/src/main/java/com/cassandra/tictacapp/MainActivity.kt
+++ app/src/main/java/com/cassandra/tictacapp/MainActivity.kt
@@ -12,26 +12,19 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 
     //pasar de una pantalla a otra
     fun nextScreen(v:View){
-        //Extraemos el nombre de los jugadores y los guardamos en las variables
-        var player1 = findViewById<EditText>(R.id.etJugador1)
-        var player2 = findViewById<EditText>(R.id.etJugador2)
+        var player1 = findViewById<EditText>(R.id.etPlayer1)
+        var player2 = findViewById<EditText>(R.id.etPlayer2)
 
-        //estoy en applicationContext y me quiero ir a la pantalla GameActivity
-        var intento = Intent(applicationContext,GameActivity::class.java)
-        //Se coloca el nombre de los jugadores en la pantalla nueva
-        intent.putExtra("player1:", player1.text.toString())
-        intent.putExtra("player2:", player2.text.toString())
+        val intent = Intent(this, GameActivity::class.java)
+        intent.putExtra("player1", player1.text.toString())
+        intent.putExtra("player2", player2.text.toString())
         startActivity(intent)
+
+
     }
 }
